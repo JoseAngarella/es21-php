38ca060b73adb5c5d1b1d85c25176e3089c20a98
Benncio09 <lorebencini13@outlook.com>
2024-10-11 08:28:17 +0200

    finito e funzionante

--- esercizio21.php
+++ esercizio21.php
@@ -4,19 +4,27 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     
     <?php
-
         function ottioniMedia($lista){
             $somma=0;
             for ($i=0; $i<count($lista);$i++){
                 $somma+=$lista[$i];
             }
-            return $somma/count($lista);
+            return  number_format(($somma/count($lista)),2) ;
         }
-
+        function ottieniMax($lista){
+            $max=$lista[0];
+            for($i=1;$i<count($lista);$i++){
+                if($lista[$i]>$max){
+                    $max=$lista[$i];
+                }
+            }
+            return $max;
+        }
         function stampaStudente($nome, $cognome, $voti){
             $stampa="<ul><li>nome : $nome</li><li>cognome : $cognome</li><li>ListaVoti : <ol>";
             for($i=0;$i<count($voti);$i++){
@@ -25,23 +33,29 @@
             $stampa=$stampa."</ol></li></ul>";
             return $stampa;
         }
-        
-        $studente1=array("nome"=>"alessio", "cognome"=>"rossi", "voti"=> array(rand(2,10),rand(2,10),rand(2,10)));
-        $studente2=array("nome"=>"mario", "cognome"=>"pratesi", "voti"=> array(rand(2,10),rand(2,10),rand(2,10)));
-        $studente3=array("nome"=>"lorenzo", "cognome"=>"mancini", "voti"=> array(rand(2,10),rand(2,10),rand(2,10)));
+        function stampaTabellaStudenti($studenti){
+            $stampa="<table style='border-collapse:collapse'><tr><th>Nome Studente</th><th>Cognome Studente</th><th>Media Voti</th><th>Voto Massimo</th></tr>";
+            foreach($studenti as $studente){
+                $stampa=$stampa."<tr><td>{$studente['nome']}</td><td>{$studente['nome']}</td><td>".
+                ottioniMedia($studente["voti"])."</td><td>".ottieniMax($studente["voti"])."</td></tr>";
+                
+            }
+            $stampa=$stampa."</table>";
+            return $stampa;
+        }
 
+        
+
+        $studente1=array("nome"=>"Alessio", "cognome"=>"Rossi", "voti"=> array(rand(2,10),rand(2,10),rand(2,10)));
+        $studente2=array("nome"=>"Mario", "cognome"=>"Pratesi", "voti"=> array(rand(2,10),rand(2,10),rand(2,10)));
+        $studente3=array("nome"=>"Lorenzo", "cognome"=>"Mancini", "voti"=> array(rand(2,10),rand(2,10),rand(2,10)));
         $studenti= array($studente1, $studente2, $studente3);
         $stampa="";
         for($i=0;$i<count($studenti);$i++){
             $stampa=$stampa."<h2>{$studenti[$i]['nome']} {$studenti[$i]['cognome']}</h2>".
             stampaStudente($studenti[$i]['nome'],$studenti[$i]['cognome'],$studenti[$i]['voti']);
         }
-
-
-        
-
-
-        
+        $stampa=$stampa."<br>".stampaTabellaStudenti($studenti);
         echo $stampa;
     ?>
 </body>
